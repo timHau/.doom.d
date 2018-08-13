2ed1fe9a64b0c512450e1792406abbf7f6becef6
timHau <tim.hau@hotmail.de>
2018-08-13 14:43:29 +0200

    change company height/width

--- +tools.el
+++ +tools.el
@@ -4,7 +4,8 @@
   (setq company-idle-delay 0.3
         company-auto-complete-chars nil
         company-show-numbers t
-        company-tooltip-minimum-width 80
+        company-tooltip-minimum-width 50
+        company-tooltip-minimum-height 60
         company-tooltip-limit 20
         company-minimum-prefix-length 2))
 
