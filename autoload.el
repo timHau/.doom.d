f44b62076e08ae8ec1376b7d80194a002eeebe39
tim hau <hau@meso.net>
2018-08-28 10:28:35 +0200

    change font to something less fancy

--- autoload.el
+++ autoload.el
@@ -11,18 +11,3 @@
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
 
-;;;###autoload
-(defun ivy-poshandler-display-at-lower-center (info)
-  (cons (/ (- (plist-get info :parent-frame-width)
-              (plist-get info :posframe-width))
-           2)
-        (/ (- (plist-get info :parent-frame-height)
-              (plist-get info :posframe-height))
-           3)))
-
-
-;;;###autoload
-(defun ivy-posframe-display-at-lower-center (str)
-  (ivy-posframe--display str #'ivy-poshandler-display-at-lower-center)
-  (ivy-posframe-enable))
-
