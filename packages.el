61d0dc10fe0c5cf38337e26fb330f71752b42eb0
timHau <tim.hau@hotmail.de>
2018-09-21 11:56:28 +0200

    remove emacs-snippets from packages

--- packages.el
+++ packages.el
@@ -1,11 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/default/packages.el
 
-(package! emacs-snippets
-  :recipe (:fetcher github
-           :repo "hlissner/emacs-snippets"
-           :files ("*")))
-
 (package! sage-shell-mode)
 (package! wttrin)
 (package! exec-path-from-shell :disable t)
