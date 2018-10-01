e3db2847cfa69396dd8f44eaea3272f2465614f1
timHau <tim.hau@hotmail.de>
2018-10-01 15:21:33 +0200

    ivy ag on M-a

--- +tools.el
+++ +tools.el
@@ -60,12 +60,7 @@
 
 ;; https://github.com/atykhonov/google-translate/issues/52#issuecomment-423870290
 (def-package! google-translate
-:config
-(when (and (string-match "0.11.14"
-(google-translate-version))
-             (>= (time-to-seconds)
-		 (time-to-seconds
-                  (encode-time 0 0 0 23 9 2018))))
-    (defun google-translate--get-b-d1 ()
-      ;; TKK='427110.1469889687'
-      (list 427110 1469889687))))
+  :config
+  (defun google-translate--get-b-d1 ()
+    ;; TKK='427110.1469889687'
+    (list 427110 1469889687)))
